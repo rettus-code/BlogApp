512ccca606fa319b3510c40a541e8c370c7350c1
rettusm <rettusm@wwu.edu>
2022-10-20 14:14:03 -0700

    it's going?

--- app.js
+++ app.js
@@ -95,7 +95,10 @@ app.all('*', function(req, res) {
   res.redirect("/post/about");
 });
 
-const server = http.createServer(app).listen(port,() => {
+const server = http.createServer({
+	key: fs.readFileSync('host.key'),
+	cert: fs.readFileSync('host.cert')
+}, app).listen(port,() => {
 console.log('Listening ...Server started on port ' + port);
 })
 
