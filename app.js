ce9af0087d8cc0e3c09e67b09112e8370114bedb
rettusm <rettusm@wwu.edu>
2022-10-22 13:58:07 -0700

    it's going?

--- app.js
+++ app.js
@@ -85,6 +85,8 @@ passport.deserializeUser(function(id, done) {
 
 app.use(function(req, res, next) {
 	res.locals.isAuthenticated=req.isAuthenticated();
+	res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  	res.header("Cross-Origin-Opener-Policy", "same-origin");
 	next();
 });
 
