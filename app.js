bc2e9a87b5d8afbb4cd7a1ce138c7b54da5f1de7
rettusm <rettusm@wwu.edu>
2022-10-26 15:41:29 -0700

    redis connect

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const config = require('./config/config');
 const compression = require ('compression');
 const helmet = require('helmet');
-const https= require("https");
+const http= require("http");
 const fs = require('fs')
 
 
@@ -97,11 +97,7 @@ app.all('*', function(req, res) {
   res.redirect("/post/about");
 });
 //app.use(enforce.HTTPS({ trustProtoHeader: true }));
-const server = https.createServer({
-	key: fs.readFileSync('host.key'),
-	cert: fs.readFileSync('host.cert')
-},app).listen(port,() => {
-console.log('Listening ...Server started on port ' + port);
-})
-
+const server = http.createServer(app.listen(port,() => { 
+	console.log('Listening ...Server started on port ' + port);
+	}))
 module.exports = app;
