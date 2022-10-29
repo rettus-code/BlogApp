4ab9777f2ccb706ae147ebd09b06b87eb2024041
cantrek2 <cantrek2@wwu.edu>
2022-10-29 01:55:06 -0700

    added replacements for mySQL database queries

---
+++ controllers/posts.controllerbeta.js
@@ -0,0 +1,41 @@
+const Post = require("../models/post");
+
+
+const homeStartingContent =
+	'The home pages lists all the blogs from all the users.';
+
+const composePost = (req, res) => {
+	const post = new Post({
+    username: req.user.username,
+		title: req.body.postTitle,
+		content: req.body.postBody
+	});
+
+	post.save();
+	res.redirect('/post');
+};
+
+const displayAllPosts = (req, res) => {
+	connection.query('SELECT * FROM post', function(err, posts) {
+		res.render('home', {
+			startingContent: homeStartingContent,
+			posts: posts
+		});
+	});
+};
+async function displayPost (req, res)  {
+	const requestedPostId = req.params.postId;
+
+	connection.query('SELECT * FROM post where _id = ? ', [requestedPostId], function(err, post) {
+		res.render('post', {
+			title: post.title,
+			content: post.content
+		});
+	});
+};
+
+module.exports = {
+	displayAllPosts,
+	displayPost,
+    composePost
+};
