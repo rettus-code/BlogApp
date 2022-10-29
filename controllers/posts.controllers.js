4b53c47b6ef08a999ea28d100fe11f6394e2a117
rettusm <rettusm@wwu.edu>
2022-10-29 16:10:52 -0700

    lazy load

--- controllers/posts.controllers.js
+++ controllers/posts.controllers.js
@@ -34,7 +34,7 @@ async function displayPost (req, res)  {
 	if(post_record == null){
 		Post.findOne({ _id: requestedPostId }, function(err, post) {
 			redisClient.set(
-				requestedPostId, '{title: "' +post.title +'" content: "' +post.content+'"}'
+				requestedPostId, '{"title:" "' +post.title +'" content:" "' +post.content+'"}'
 			)
 			res.render('post', {
 				title: post.title,
