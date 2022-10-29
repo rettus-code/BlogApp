b3eee56788ba7f9c7ac6522387ce9c5b9714a513
rettusm <rettusm@wwu.edu>
2022-10-29 15:50:19 -0700

    lazy load

--- controllers/posts.controllers.js
+++ controllers/posts.controllers.js
@@ -42,7 +42,7 @@ async function displayPost (req, res)  {
 			});
 		});
 	} else {
-		console.log(post_record.title);
+		console.log(post_record[0].title);
 		res.render('post',{
 			title: post_record.title,
 			content: post_record.content
