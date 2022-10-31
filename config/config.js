770345bad412e268d2b6bc2fc92715b6603a9789
rettusm <rettusm@wwu.edu>
2022-10-31 16:42:47 -0700

    cleanup first pass

--- config/config.js
+++ config/config.js
@@ -28,7 +28,7 @@ var config = convict({
   redis_host: {
     doc: 'redis hostname',
     format: String,
-    default: "cachetest.yrwyfy.ng.0001.usw2.cache.amazonaws.com",
+    default: "",
     env: 'REDIS_HOST',
     arg: 'redishost'
   },
