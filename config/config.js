857937e5f9e2a9e7ea17800555b0badddbf3e15e
rettusm <rettusm@wwu.edu>
2022-10-31 16:12:26 -0700

    cleanup first pass

--- config/config.js
+++ config/config.js
@@ -46,7 +46,7 @@ var config = convict({
     },
     db_url: {
       format: '*',
-      default: "",
+      default: "mongodb+srv://rettusm:TRashcanman2@cluster0.1pgel8d.mongodb.net/",
       env: 'DB_URL'
     },
     password: {
